e06c2280fa015d9a276a255fd7240b9c9750c248
James Whiteley <31282758+james-work-account@users.noreply.github.com>
2020-10-13 17:48:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def make_session_permanent():
 
 @app.after_request
 def add_header(response):
-    response.cache_control.max_age = 0
+    response.cache_control.max_age = 300
     return response
 
 
