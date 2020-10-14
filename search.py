ff6c0d6628f1fe2d1f02e8f103c60853b763a558
james-work-account <31282758+james-work-account@users.noreply.github.com>
2020-10-14 18:23:36 +0100

    chromedriver options

--- search.py
+++ search.py
@@ -30,6 +30,8 @@ class Search:
             'user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36')
         self.options.add_argument('--disable-gpu')
         self.options.add_argument('--no-sandbox')
+        self.options.add_argument('--disable-extensions')
+        self.options.add_argument('--disable-dev-shm-usage')
         self.options.headless = True
         self.driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=self.options)
         self.shops = [
